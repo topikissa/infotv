03a7c1b859f479ca14b62653fc74740012ba896c
Santeri Hiltunen <hiltunen.santeri@gmail.com>
2016-03-28 16:24:58 +0300

    Generate source maps and add trailing commas to webpack config

--- infotv/frontend/webpack.config.js
+++ infotv/frontend/webpack.config.js
@@ -8,30 +8,31 @@ module.exports = {
     context: __dirname,
     entry: "./src/main.js",
     bail: true,
+    devtool: "source-map",
     output: {
         path: outputFsPath,
         filename: "bundle.js",
-        publicPath: outputPublicPath
+        publicPath: outputPublicPath,
     },
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
+                loader: 'babel',
             },
             {
                 test: /\.(woff|svg|otf|ttf|eot|png)(\?.*)?$/,
-                loader: 'url'
-            }
-        ]
+                loader: 'url',
+            },
+        ],
     },
     resolve: {
         alias: {
-            "current-style": "../styles/" + CURRENT_STYLE + "/less/style.less"
-        }
+            "current-style": "../styles/" + CURRENT_STYLE + "/less/style.less",
+        },
     },
     plugins: [
         new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en|fi/)
-    ]
+    ],
 };
